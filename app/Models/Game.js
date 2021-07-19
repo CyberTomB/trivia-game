a14c1650cfbbe87f775c5ec68c1591d2a89c2b69
CyberDack <afntwrk.guyrpg@gmail.com>
2021-07-19 16:06:21 -0600

    added game class

--- app/Models/Game.js
+++ app/Models/Game.js
@@ -1,3 +1,10 @@
+import { generateId } from "../Utils/GenerateId";
+
 export default class Game {
 
+   constructor(id = generateId(), questions, index = 0) {
+      this.id = id
+      this.questions = questions
+      this.index = index
+   }
 }
