1c1ffd59b947d41e7c4f642a912816ee2ddf956b
CyberDack <afntwrk.guyrpg@gmail.com>
2021-07-19 15:18:06 -0600

    functional buttons on page

--- app/Models/Question.js
+++ app/Models/Question.js
@@ -1,5 +1,31 @@
 export default class Question {
-   constructor({ }) {
+   constructor({ question, correct_answer, incorrect_answers, id }) {
+      this.question = question
+      this.correct = correct_answer
+      this.answers = incorrect_answers
 
+      let randIndex = Math.floor(Math.random() * (incorrect_answers.length + 1))
+      this.answers.splice(randIndex, 0, correct_answer)
+   }
+
+   get Template() {
+      return `
+      <div class="col-6 card text-center">
+         <div class="card-title">${this.question}</div>
+            <div class="card-body">
+               ${this.TemplateQs}
+            </div>
+      </div>
+      `
+   }
+
+   get TemplateQs() {
+      let template = ''
+      this.answers.forEach(a => {
+         console.log(a)
+         template += `<button onclick="console.log('${a}')">${a}</button>`
+      })
+      return template
    }
 }
+
--- app/Services/QuestionsSerivce.js
+++ app/Services/QuestionsSerivce.js
@@ -1,10 +1,20 @@
+import { ProxyState } from "../AppState.js";
+import Question from "../Models/Question.js";
 import { api } from "./AxiosService.js";
 
 class QuestionsSerivce {
    async getQuestion() {
       const res = await api.get()
 
-      console.log(res.data)
+      console.log('raw question: ', res.data.results)
+
+      let questions = res.data.results.map(q => new Question(q))
+      ProxyState.trivia = questions
+   }
+
+
+   checkAnswer(answer) {
+      if (answer == )
    }
 }
 
